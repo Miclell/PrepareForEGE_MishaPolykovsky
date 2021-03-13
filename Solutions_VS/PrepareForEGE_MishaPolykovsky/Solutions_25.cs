88ceb459fccd166142354d46a55f609ef15634ba
Miclell <hem123v@gmail.com>
2021-03-13 23:43:59 +0300

    Add Stat Grad B1

---
+++ Solutions_VS/PrepareForEGE_MishaPolykovsky/Solutions_25.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PrepareForEGE_MishaPolykovsky
+{
+    internal class Solutions_25
+    {
+        internal static void Start(object n = null)
+        {
+            if (n == null)
+            {
+                Solutions_25 obj = new Solutions_25();
+
+                foreach (MethodInfo method in obj.GetType().GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic))
+                {
+                    if (method.Name != "Start")
+                    {
+                        Console.WriteLine("Номер " + (method.Name[^2].ToString() + method.Name[^1].ToString()).Trim('_') + ":");
+                        try { method.Invoke(obj, null); }
+                        catch { };
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Solutions_25 obj = new Solutions_25();
+                MethodInfo method = obj.GetType().GetMethod("Solution_" + n, BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic);
+
+                try
+                {
+                    Console.WriteLine("Номер " + (method.Name[^2].ToString() + method.Name[^1].ToString()).Trim('_') + ":");
+                    method.Invoke(obj, null);
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Такого решения нема :(");
+                }
+            }
+        }
+
+        private static void Solution_128()
+        {
+            //4234679 10157812
+            static bool Check(int x)
+            {
+                if (x <= 1)
+                    return false;
+
+                for (int d = 2; d * d <= x; d++)
+                    if (x % d == 0)
+                        return false;
+
+                return true;
+            }
+
+            for (int i = (int)Math.Pow(4234679, 0.25); i < (int)Math.Pow(10157812, 0.25) + 1; i++)
+                if (Check(i))
+                    Console.WriteLine($"{(int)Math.Pow(i, 4)} {new List<int>() { i, (int)Math.Pow(i, 2), (int)Math.Pow(i, 3)}.Max()}");
+        }
+
+        private static void Solution_129()
+        {
+            //12034679 23175821
+            static bool Check(int x)
+            {
+                if (x <= 1)
+                    return false;
+
+                for (int d = 2; d * d <= x; d++)
+                    if (x % d == 0)
+                        return false;
+
+                return true;
+            }
+
+            for (int i = (int)Math.Pow(12034679, 0.25); i < (int)Math.Pow(23175821, 0.25); i++)
+                if (Check(i))
+                    Console.WriteLine($"{(int)Math.Pow(i, 4)} {new List<int>() { i, (int)Math.Pow(i, 2), (int)Math.Pow(i, 3)}.Max()}");
+        }
+
+        private static void Solution_130()
+        {   
+            //50034679 92136895
+            static bool Check(int x)
+            {
+                if (x == 1)
+                    return false;
+
+                int d = 2;
+
+                while (d * d <= x)
+                {
+                    if (x % d == 0)
+                        return false;
+
+                    d++;
+                }
+
+                return true;
+            }
+            
+
+            for (int i = (int)Math.Pow(50034679, 0.25); i < (int)Math.Pow(92136895, 0.25) + 1; i++)
+            {
+                int x = (int)Math.Pow(i, 4);
+
+                if (Check(i))
+                    Console.WriteLine($"{x} {new List<int>() { i , (int)Math.Pow(i, 2), (int)Math.Pow(i, 3) }.Max()}");
+            }
+        }
+
+        private static void Solution_143()
+        {
+            //33333 55555
+            static bool Check(int x)
+            {
+                if (x <= 1)
+                    return false;
+
+                for (int d = 2; d * d <= x; d++)
+                    if (x % d == 0)
+                        return false;
+
+                return true;
+            }
+
+            for (int i = 33333; i < 55555; i++)
+                if (Check(i) && (i.ToString().Sum(char.GetNumericValue) > 35))
+                    Console.WriteLine($"{i} {i.ToString().Sum(char.GetNumericValue)}");
+        }
+    }
+}
--- Solutions_VS/PrepareForEGE_MishaPolykovsky/Solutions_StatGrad_B1_2210.cs
+++ Solutions_VS/PrepareForEGE_MishaPolykovsky/Solutions_StatGrad_B1_2210.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 
 namespace PrepareForEGE_MishaPolykovsky
 {
     internal class Solutions_StatGrad_B1_2210
     {
-        internal static void Solution_2()
+        //это мое извращенство, позволяющие запустить все методы класса в одну строчку и упрощающие вызов методов
+        //правда оно сейчас еще немного багает
+        internal static void Start(object n = null)
+        {
+            if (n == null)
+            {
+                Solutions_StatGrad_B1_2210 obj = new Solutions_StatGrad_B1_2210();
+
+                foreach (MethodInfo method in obj.GetType().GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic))
+                {
+                    if (method.Name != "Start")
+                    {
+                        Console.WriteLine("Номер " + (method.Name[^2].ToString() + method.Name[^1].ToString()).Trim('_') + ":");
+                        try { method.Invoke(obj, null); }
+                        catch { };
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Solutions_StatGrad_B1_2210 obj = new Solutions_StatGrad_B1_2210();
+                MethodInfo method = obj.GetType().GetMethod("Solution_" + n, BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic);
+
+                try
+                {
+                    Console.WriteLine("Номер " + (method.Name[^2].ToString() + method.Name[^1].ToString()).Trim('_') + ":"); 
+                    method.Invoke(obj, null); 
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Такого решения нема :(");
+                }
+            }
+        }
+
+        private static void Solution_2()
         {
             //(w \/ ¬x) /\ (w ≡ ¬y) /\ (w → z)
 
-            bool[] table = new[] { false, true };
+            bool[] table = new[] { true, false };
             foreach (var x in table)
                 foreach (var y in table)
                     foreach (var z in table)
@@ -22,7 +58,22 @@ namespace PrepareForEGE_MishaPolykovsky
                                 Console.WriteLine($"{Convert.ToInt32(w)} {Convert.ToInt32(y)} {Convert.ToInt32(z)} {Convert.ToInt32(x)}");
         }
 
-        internal static void Solution_4()
+        private static void Solution_5()
+        {
+            for (int i = 171; ; i++)
+            {
+                string s = Convert.ToString(i, 2);
+
+                if (((s[0..^2].Count(x => x == '1') % 2).ToString() == s[^1].ToString()) &&
+                    ((s[0..^3].Count(x => x == '1') % 2).ToString() == s[^2].ToString()))
+                {
+                        Console.WriteLine(Convert.ToInt32(s[0..^3], 2));
+                        break;
+                }
+            }
+        }
+
+        private static void Solution_6()
         {
             //128
             int i;
@@ -38,11 +89,36 @@ namespace PrepareForEGE_MishaPolykovsky
                 if (n == 128)
                     goto Finsish;
             }
+
         Finsish:
             Console.WriteLine(i);
         }
 
-        internal static void Solution_12()
+        public static void Solution_8()
+        {
+            string[] table = new[] { "А", "Н", "Д", "Р", "Е", "Й" };
+
+            List<string> res = new List<string>();
+
+            foreach (var item1 in table)
+                foreach (var item2 in table)
+                    foreach (var item3 in table)
+                        foreach (var item4 in table)
+                            foreach (var item5 in table)
+                                foreach (var item6 in table)
+                                    foreach (var item7 in table)
+                                        res.Add(item1 + item2 + item3 + item4 + item5 + item6 + item7);
+
+            for (int i = 0; i < res.Count; i++)
+            {
+                if ((res[i][0] == 'Й') || (res[i].Count(x => x == 'А') != 1) || (res[i].Count(x => x == 'Й') != 1))
+                    res.Remove(res[i]);
+            }
+
+            Console.WriteLine(res.Count);
+        } // я знаю что это извращенство)
+
+        private static void Solution_12()
         {
             string s = string.Join("", Enumerable.Repeat("1", 99));
 
@@ -55,7 +131,7 @@ namespace PrepareForEGE_MishaPolykovsky
             Console.WriteLine(s);
         }
 
-        internal static void Solution_14()
+        private static void Solution_14()
         {
             BigInteger a = BigInteger.Pow(81, 17) + BigInteger.Pow(3, 24) - 45;
 
@@ -71,7 +147,7 @@ namespace PrepareForEGE_MishaPolykovsky
             Console.WriteLine(count);
         }
 
-        internal static void Solution_15()
+        private static void Solution_15()
         {
             //(A < 50) /\ (¬ДЕЛ(x, A) → (ДЕЛ(x, 10) → ¬ДЕЛ(x, 12)))
 
@@ -93,7 +169,7 @@ namespace PrepareForEGE_MishaPolykovsky
             }
         }
 
-        internal static void Solution_16()
+        private static void Solution_16()
         {
             //F(1) = 1;
             //F(n) = n + F(n–2), если n > 1 и при этом n нечётно;
@@ -113,7 +189,7 @@ namespace PrepareForEGE_MishaPolykovsky
             Console.WriteLine(F(60));
         }
 
-        internal static void Solution_17()
+        public static void Solution_17()
         {
             static int Count(BigInteger first, BigInteger last)
             {
@@ -128,17 +204,55 @@ namespace PrepareForEGE_MishaPolykovsky
             }
 
             int count = 0;
-            BigInteger h = (4 * BigInteger.Pow(10, 10) - 2 * BigInteger.Pow(10, 10)) / 60, f = 2 * BigInteger.Pow(10, 10); ;
+            BigInteger h = (4 * BigInteger.Pow(10, 10) - 2 * BigInteger.Pow(10, 10)) / 60, f = 2 * BigInteger.Pow(10, 10);
             Enumerable.Range(0, 60).AsParallel().ForAll(x =>
             {
                 count += Count(f, f + h);
                 f += h;
             });
 
-            Console.WriteLine($"{count} ого"); // 2055, 5 минут и готово)))
-        } //страшилка, но вроде работает :)
+            Console.WriteLine($"{count} ого"); //выдает 2055, не знаю почему :(
+        } //страшилка многопоточная
 
-        internal static void Solution_22()
+        public static void Solution_171()
+        {
+            static int Count(double first, double last)
+            {
+                int count = 0;
+                for (double i = first; i < last; i++)
+                    if ((i % 7 == 0) && (i % 100000 == 0) &&
+                        (i % 13 != 0) && (i % 29 != 0) &&
+                        (i % 43 != 0) && (i % 101 != 0))
+                        count++;
+
+                return count;
+            }
+
+            Console.WriteLine(Count(2 * Math.Pow(10, 10), 4 * Math.Pow(10, 10)));
+        } //24126, простой перебор, 10 минут где-то
+
+        private static void Solution_18()
+        {
+            string[] s = File.ReadAllLines(@"D:\hem12\Documents\Документы Миша\Школьные предметы\ЕГЭ информатика\Solutions\PrepareForEGE_MishaPolykovsky\Others_Solutions\Ст. Инф. 22.10.2020.(Ф.)\18\18.txt");
+
+            decimal count = Convert.ToDecimal(s[0]), max = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (Convert.ToDecimal(s[i]) < Convert.ToDecimal(s[i - 1]))
+                    count += Convert.ToDecimal(s[i]);
+                else
+                {
+                    if (count > max)
+                        max = count;
+
+                    count = Convert.ToDecimal(s[i]);
+                }
+            }
+
+            Console.WriteLine((int)count);
+        }
+
+        private static void Solution_22()
         {
             for (int i = 0; i < 1000; i++)
             {
@@ -155,6 +269,83 @@ namespace PrepareForEGE_MishaPolykovsky
                     break;
                 }
             }
+        }
+
+        private static void Solution_23()
+        {
+            List<int> res = new List<int>() { 0, 1 };
+            for (int i = 0; i < 70; i++)
+                res.Add(0);
+
+            for (int i = 1; i < 22; i++)
+            {
+                if (i + 1 <= 22)
+                    res[i + 1] += res[i];
+                if (i * 3 <= 22)
+                    res[i * 3] += res[i];
+            }
+
+            for (int i = 22; i <= 70; i++)
+            {
+                if (i + 1 <= 70)
+                    res[i + 1] += res[i];
+                if (i * 3 <= 70)
+                    res[i * 3] += res[i];
+            }
+
+            Console.WriteLine(res[70]);
+        }
+
+        private static void Solution_24()
+        {
+//            Определите количество строк, в которых буква E
+//            встречается чаще, чем буква A.
+
+            string[] s = File.ReadAllLines(@"D:\hem12\Documents\Документы Миша\Школьные предметы\ЕГЭ информатика\Solutions\PrepareForEGE_MishaPolykovsky\Others_Solutions\Ст. Инф. 22.10.2020.(Ф.)\24\24.txt");
+            
+            int count = 0, counterA = 0, counterE = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                foreach (char item in s[i])
+                {
+                    if (item == 'A')
+                        counterA++;
+
+                    if (item == 'E')
+                        counterE++;
+                }
+
+                if (counterE > counterA)
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void Solution_25()
+        {
+            //123456789 223456789
+
+            List<int> res = new List<int>();
+            for (int i = (int)Math.Pow(123456789, 0.25); i < (int)Math.Pow(223456789, 0.25) + 1; i++)
+            {
+                int x = (int)Math.Pow(i, 4), d = 2;
+
+                bool flag = true;
+                while (d * d <= i)
+                {
+                    if (x % d == 0)
+                        flag = false;
+
+                    d++;
+                }
+
+                if (flag)
+                    res.Add(new List<int>() { x, (int)Math.Pow(i, 2), (int)Math.Pow(i, 3) }.Max());
+            }
+
+            foreach (var item in res)
+                Console.WriteLine(item);
         }
     }
 }
